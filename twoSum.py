122a2cb1aa9a97c518a1162cff527a74381a39d3
mariawu2016 <pistilwu@qq.com>
2016-12-13 12:31:39 +0800

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -14,8 +14,19 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 """
-#  
+#list很小的时候适用，逻辑简单
 def twoSum(nums,target):
+    m = len(nums)
+    li = []
+    for i in range(0,m):
+        for j in range(m-i,m):
+            if nums[j] == target-nums[i] and i < j:
+                li.append(i)
+                li.append(j)
+    return li
+    
+#list很大的时候适用，效率高，Python的dic用hash表实现
+def twoSum2(nums,target):
     n=len(nums)
     li=[]
     res=[]
@@ -32,24 +43,18 @@ def twoSum(nums,target):
             res.append(re)
     return res
     
-def twoSum2(nums,target):
-    m = len(nums)
-    li = []
-    for i in range(0,m):
-        for j in range(0,m):
-            if nums[j] == target-nums[i] and i < j:
-                li.append(i)
-                li.append(j)
-    return li
 
 def main(args):
-    print(twoSum2([3,2,4],6))
     li=[]
     for i in range(0,10000,2):
         li.append(i)
     #print li
-    print(twoSum(li,9990))
+    print(twoSum(li,19992))
+    print(twoSum2([3,2,4],6))
     return 0
+#result:
+#[[4997, 4999]]
+#[1, 2]
 
 if __name__ == '__main__':
     import sys
