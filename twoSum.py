c68e9b17301a9dfa8dabaacb43ca050be1f9836e
mariawu2016 <pistilwu@qq.com>
2016-12-13 12:16:33 +0800

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -10,12 +10,30 @@ class Solution(object):
     Because nums[0] + nums[1] = 2 + 7 = 9,
     return [0, 1].
     """
-    def twoSum(self, nums, target):
-        m = len(nums) - 1
+    #if the list is small
+    def twoSum(nums, target):
+        m = len(nums)
         li = []
-        for i in range(m, 0, -1):
-            for j in range(0, m):
-                if nums[i] + nums[j] == target and i > j:
-                    li.append(j)
+        for i in range(0,m):
+            for j in range(0,m):
+                if nums[j] == target-nums[i] and i < j:
                     li.append(i)
+                    li.append(j)
         return li
+    
+    #if the list is much bigger
+    #dict in python used Hash
+    def twoSum(nums,target):
+        n=len(nums)
+        li=[]
+        re=[]
+        for i in range(0,n):
+           li.append((nums[i],i))        
+        d=dict(li)
+        #print(d)
+        for j in range(0,n):
+            result=target-nums[j]
+            if d.has_key(result) and d[result]>j:
+                re.append(j)
+                re.append(d[result])
+        return re
